aa7efbc394f71f186ff8873968bbbebe03e37076
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2023-10-24 18:38:14 -0500

    Initial Commit

---
+++ CoaquiraColeccionesDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoaquiraColeccionesDeJuegos
+//
+//  Created by Leonardo Vittori Coaquira Coillo on 10/24/23.
+//  Copyright © 2023 Leonardo Vittori Coaquira Coillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
